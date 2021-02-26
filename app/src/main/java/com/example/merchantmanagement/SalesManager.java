80c2e4668a3c913d8166601035b3031cc430d9b2
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-02-26 11:40:00 +0600

    Added Sales Manager

---
+++ app/src/main/java/com/example/merchantmanagement/SalesManager.java
@@ -0,0 +1,43 @@
+package com.example.merchantmanagement;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 9/1/18.
+ */
+
+public class SalesManager {
+    private String name, number, password, email, orgName;
+    private ArrayList<InventoryItem> inventoryItems;
+
+    public SalesManager(){}
+    public SalesManager(String name, String number, String password, String email, String orgName){
+        this.name = name;
+        this.number = number;
+        this.password = password;
+        this.email = email;
+        this.orgName = orgName;
+        this.inventoryItems  = new ArrayList<>();
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
