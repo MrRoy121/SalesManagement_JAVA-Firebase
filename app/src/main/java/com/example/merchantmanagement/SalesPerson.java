42f017fc1ec5f9f03296ba2947e866142d374923
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-02-27 11:40:00 +0600

    Added Sales Person

---
+++ app/src/main/java/com/example/merchantmanagement/SalesPerson.java
@@ -0,0 +1,49 @@
+package com.example.merchantmanagement;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 9/1/18.
+ */
+
+public class SalesPerson {
+    private String name, number, managerName, password, emailId;
+    private ArrayList<InventoryItem> inventoryItems;
+
+    public SalesPerson(){}
+    public SalesPerson(String name, String number, String password, String managerName, String Email) {
+        this.name = name;
+        this.number = number;
+        this.password = password;
+        this.managerName = managerName;
+        this.emailId = Email;
+
+        this.inventoryItems = new ArrayList<>();
+
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public ArrayList<InventoryItem> getInventoryItems() {
+        return inventoryItems;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
