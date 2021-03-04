0495892309a60f570d630c1b1bf42f723727ac50
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-03-04 07:00:00 +0600

    Added Upload

---
+++ app/src/main/java/com/example/merchantmanagement/Upload.java
@@ -0,0 +1,23 @@
+package com.example.merchantmanagement;
+
+public class Upload {
+
+    private String email, url;
+
+    public Upload(){
+
+    }
+    public Upload(String email, String url){
+        this.email = email;
+        this.url = url;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+}
