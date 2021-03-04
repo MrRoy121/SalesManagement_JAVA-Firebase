c761cb8690d5e70d8ecb728f18a17c51611a0d5e
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-03-04 15:00:00 +0600

    Added Adapter

---
+++ app/src/main/java/com/example/merchantmanagement/SalespersonInventoryAdapter.java
@@ -0,0 +1,71 @@
+package com.example.merchantmanagement;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mehul Garg on 22-09-2018.
+ */
+
+public class SalespersonInventoryAdapter extends RecyclerView.Adapter<SalespersonInventoryAdapter.MyViewHolder> {
+
+    private ArrayList <InventoryItem> list;
+    private Context context;
+
+    public SalespersonInventoryAdapter(){
+
+    }
+
+    public SalespersonInventoryAdapter(Context context,ArrayList<InventoryItem> list){
+        this.list=list;
+        this.context=context;
+    }
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView= LayoutInflater.from(parent.getContext()).inflate(R.layout.inventory_item_salesperson,parent,false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        holder.itemName.setText(list.get(position).getItemName());
+        holder.sold.setText(String.valueOf(list.get(position).getSold()));
+        holder.progressBar.setProgress(list.get(position).getSold());
+        holder.remaining.setText(String.valueOf(list.get(position).getTotal_available()));
+        holder.profitText.setText(String.valueOf(list.get(position).getSold() * list.get(position).getProfit()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView itemName, sold, remaining, profitText;
+        private ImageView edit;
+        private ProgressBar progressBar;
+
+        public MyViewHolder(View itemView){
+            super(itemView);
+            itemName=itemView.findViewById(R.id.item_name);
+            sold=itemView.findViewById(R.id.items_sold);
+            progressBar=itemView.findViewById(R.id.progressBar2);
+            remaining=itemView.findViewById(R.id.remaining_items_text);
+            profitText=itemView.findViewById(R.id.profitText);
+        }
+    }
+}
