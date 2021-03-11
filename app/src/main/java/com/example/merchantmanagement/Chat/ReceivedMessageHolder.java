677450f800981f8c7621d1fcd8db294ad9ef09d2
Nilashish Roy <nilashishroyjoy@gmail.com>
2021-03-11 22:00:00 +0600

    Added Message

---
+++ app/src/main/java/com/example/merchantmanagement/Chat/ReceivedMessageHolder.java
@@ -0,0 +1,52 @@
+package com.example.merchantmanagement.Chat;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.merchantmanagement.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+/**
+ * Created by user on 9/26/18.
+ */
+
+public class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+
+    TextView messageText, timeText, nameText;
+
+    ReceivedMessageHolder(View itemView) {
+        super(itemView);
+        messageText = (TextView) itemView.findViewById(R.id.text_message_body);
+        timeText = (TextView) itemView.findViewById(R.id.text_message_time);
+        nameText = (TextView) itemView.findViewById(R.id.text_message_name);
+    }
+
+    void bind(BaseMessage message) {
+        messageText.setText(message.getMessage());
+
+        timeText.setText(getDateCurrentTimeZone(Long.parseLong(message.getCreatedAt())));
+        nameText.setText(message.getSenderName());
+
+    }
+
+    public  String getDateCurrentTimeZone(long timestamp) {
+        try{
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeInMillis(timestamp * 1000);
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy, HH:mm");
+            Date currenTimeZone = (Date) calendar.getTime();
+            return sdf.format(currenTimeZone);
+        }catch (Exception e) {
+        }
+        return "";
+    }
+}
